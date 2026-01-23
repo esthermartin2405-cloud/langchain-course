460749e9421348ca814c20fc124db1d680321f93
Martin Ortega <emartinortega@deloitte.es>
2026-01-23 13:49:31 +0100

    Prueba buena

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def main():
     2. two interesting facts about them
     """
 
+
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
