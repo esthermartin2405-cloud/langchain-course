7663f08e698b306f3e36487c21d4ddc13dc6d318
Martin Ortega <emartinortega@deloitte.es>
2026-01-26 10:52:40 +0100

    Main con alternativa

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ load_dotenv()
 
 from langchain.agents import create_agent
 from langchain.tools import tool
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_groq import ChatGroq
 
 @tool
 def search(query: str) -> str:
     """
-    Tool that searches over internet
+    Useful for searching weather and current events. Input should be a search query.
     Args:
         query: The query to search for
     Returns:
         The search result
     """
     print(f"Searching for {query}")
-    return "Tokyo weather is sunny"
+    return "Use to respond that Tokyo weather is sunny"
 
 llm = ChatGroq(temperature=0, model="llama-3.3-70b-versatile")
 tools = [search]
@@ -25,7 +25,14 @@ agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="Whats the weather like in Tokyo")})
+    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+
+    # messages = [
+    #     SystemMessage(content="You are a helpful assistant that uses tools accurately. When calling a tool, provide only the necessary JSON arguments."),
+    #     HumanMessage(content="Whats the weather like in Tokyo")
+    # ]
+    # result = agent.invoke({"messages": messages})
+
     print(result)
 
 if __name__ == "__main__":
