3732dc83c693c742d23e894d38c2746434491127
Martin Ortega <emartinortega@deloitte.es>
2026-01-27 12:41:40 +0100

    Main que funciona!

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ def search(query: str) -> str:
         The search result
     """
     print(f"Searching for {query}")
-    return "Use to respond that Tokyo weather is sunny"
+    return "The current weather in Tokyo is sunny, around 25°C, with light wind."
 
-llm = ChatGroq(temperature=0, model="llama-3.3-70b-versatile")
+llm = ChatGroq(temperature=0.3, model="llama-3.1-8b-instant")
 tools = [search]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    # result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
 
-    # messages = [
-    #     SystemMessage(content="You are a helpful assistant that uses tools accurately. When calling a tool, provide only the necessary JSON arguments."),
-    #     HumanMessage(content="Whats the weather like in Tokyo")
-    # ]
-    # result = agent.invoke({"messages": messages})
+    messages = [
+        SystemMessage(content="You are a helpful assistant that uses tools accurately. When calling a tool, provide only the necessary JSON arguments."),
+        HumanMessage(content="Whats the weather like in Tokyo")
+    ]
+    result = agent.invoke({"messages": messages})
 
     print(result)
 
